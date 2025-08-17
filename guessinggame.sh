53210bcad50ff5f3ba2a106fa6e0ece2cbb3711a
VishnuP <vishnudas.parande@neurealm.com>
2025-08-17 17:58:37 -0500

    guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,1 +1,31 @@
+#!/bin/bash
+function file_cnt{
+echo $(ls -l | wc -l)
+}
 
+echo "Please guess how many files in current directory"
+
+#calling function to get count
+file_cnt=$(file_cnt)
+
+#starting guessing loop
+
+while true; 
+do
+read -p "whats your guess: " guess
+
+if ![[ "$guess" =~ ^[0-9]+$ ]]; then
+echo "not a valid number, please enter a valid number"
+continue
+fi
+
+if [ "$guess" -lt "$file_count" ]; then
+echo "not a match, is less , try again"
+elif [ "$guess" -lt "$file_count" ]; then
+echo "not a match, is greater , try again"
+else
+echo "thats it, its a match, the count is correct"
+break
+fi
+
+done
