4036d7d954362f455a3be4462b581cbccacdbe2f
VishnuP <vishnudas.parande@neurealm.com>
2025-08-20 11:54:33 -0500

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,30 +1,29 @@
 #!/bin/bash
-
 # Function to count files in current directory
-file_count() {
+function file_count() {
     echo $(ls -lrt | wc -l)
 }
 
 # This Function is to prompt user for the guess
-function fun_guess(){
+function fun_guess() {
     echo "Lets guess how many files are in the current directory:"
     read guess
 }
 
 # Main loop for guessing game
-function game(){
+function game() {
     actual_count=$(file_count)
-    echo "Welcome to the game!"
+    echo "Welcome to the guessing game!"
     fun_guess
 
     while [[ $guess -ne $actual_count ]]
     do
         if [[ $guess -lt $actual_count ]]
         then
-            echo "Too low. Try again."
+            echo "Guess is too low. Try again."
         elif [[ $guess -gt $actual_count ]]
         then
-            echo "Too high. Try again."
+            echo "Guess is too high. Try again."
         fi
         fun_guess
     done
